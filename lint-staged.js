3b9a9c7810eb46166feab65efc7214778db2195b
Rempart Irien <rempart.irien@gmail.com>
2022-05-22 22:55:37 +0200

    Test source files only

--- lint-staged.js
+++ lint-staged.js
@@ -1,4 +1,5 @@
 module.exports = {
   "*.{js,jsx,ts,tsx,json,css,js}": ["prettier --check"],
-  "*.{js,jsx,ts,tsx}": ["eslint --max-warnings=0", "npm run test"],
+  "*.{js,jsx,ts,tsx}": ["eslint --max-warnings=0"],
+  "src/**/*.{js,jsx,ts,tsx}": ["npm run test"],
 };
