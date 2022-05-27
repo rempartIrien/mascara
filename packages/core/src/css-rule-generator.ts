f0d10faebc5f51d52c90768a40af5df4d2f356a5
Rempart Irien <rempart.irien@gmail.com>
2022-05-27 13:19:22 +0200

    Add a basic rule generator

---
+++ packages/core/src/css-rule-generator.ts
@@ -0,0 +1,16 @@
+import { Properties } from "./css-types";
+
+export default function generateCssRuleString(
+  ruleName: string,
+  styleProperties: Properties,
+): string {
+  // FIXME: We assume we have a simple flat object for now.
+  const properties = Object.entries(styleProperties).map(([key, value]) => {
+    return `${toKebabCase(key)}: ${String(value)};`;
+  });
+  return `.${ruleName} { ${properties.join("")} }`;
+}
+
+function toKebabCase(s: string): string {
+  return s.replace(/([A-Z])/gm, "-$1").toLowerCase();
+}
