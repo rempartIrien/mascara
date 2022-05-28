c79135d7b3de32cb2ade3b1c72a7f21ed6b03619
Rempart Irien <rempart.irien@gmail.com>
2022-05-28 12:43:15 +0200

    Add testing-library and test staged files when committing

--- lint-staged.js
+++ lint-staged.js
@@ -1,5 +1,5 @@
 module.exports = {
   "*.{js,jsx,ts,tsx,json,css,js}": ["prettier --check"],
   "*.{js,jsx,ts,tsx}": ["eslint --max-warnings=0"],
-  "src/**/*.{js,jsx,ts,tsx}": ["npm run test"],
+  "**/src/**/*.{js,jsx,ts,tsx}": ["npm run test:staged"],
 };
